1990665e3b6ebb822311a51a7f9c02d3aaeb6af7
Joni Savage <dhairyarathibhandari@gmail.com>
2026-04-05 15:33:54 +0530

    Initial filings.

--- Lock.sh
+++ Lock.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-numbgs=$(ls /home/santiagorivera/Z/Photograms/Backgrounds | wc -l)
-prefix="/home/santiagorivera/Z/Photograms/Backgrounds/"
+numbgs=$(ls $HOME/Z/Photograms/Backgrounds | wc -l)
+prefix="$HOME/Z/Photograms/Backgrounds/"
 suffix=".png"
 
 lockscreen=$prefix$(printf "%02d" $((1 + $RANDOM % $numbgs)))$suffix
 
-swaylock \
-    -f\
-    -i $lockscreen\
-    --effect-greyscale
+swaylock -f -i $lockscreen
--- TimeGen.py
+++
@@ -1,1 +0,0 @@
-/home/santiagorivera/Z/Programs/PythonScripts/TimeGen/TimeGen.py
